e25e2356c8e4cdd2e5d83023cdc65ad695c71d12
GN15H <cristobalochoacadavid@gmail.com>
2025-09-01 17:52:26 -0500

    fixed comparison operator parameter in bubblesort

--- Taller1/Punto1/BubbleSort.hpp
+++ Taller1/Punto1/BubbleSort.hpp
@@ -25,14 +25,14 @@ public:
         int cond=0;
         for(int i=0; i<size-1; i++){
             for(int j=0; j<size-1; j++){
-                if(comp(1,0)){
-                    if(comp(*(begin+j),*(begin+j+1))){
+                if(comp(0,1)){
+                    if(!comp(*(begin+j),*(begin+j+1))){
                         cond = *(begin+j);
                         *(begin+j) = *(begin+j+1);
                         *(begin+j+1) = cond;
                     }
                 }else{
-                    if(comp(begin[j], begin[j+1])){
+                    if(!comp(begin[j], begin[j+1])){
                         cond = *(begin+j+1);
                         *(begin+j+1) = *(begin+j);
                         *(begin+j) = cond;
--- Taller1/Punto1/main.cpp
+++ Taller1/Punto1/main.cpp
@@ -4,15 +4,29 @@
 #include <array>
 #include <string>
 
+void printArr(const std::array<int, 9>& arr){
+    for(int e: arr){
+        std::cout<<e<<", ";
+    }
+    std::cout<<std::endl;
+}
+
 int main(){
-    std::array<int, 9> arr{2,55,7,3,1,0,-20,-19,80};
-    InsertionSort<int> alg;
-    alg.sort(arr.begin(), arr.end(),[](int a, int b){
+    std::array<int, 9> arr1{2,55,7,3,1,0,-20,-19,80};
+    std::array<int, 9> arr2{2,55,7,3,1,0,-20,-19,80};
+    std::array<int, 9> arr3{2,55,7,3,1,0,-20,-19,80};
+    InsertionSort<int> insertion;
+    BubbleSort<int> bubble;
+    QuickSort<int> quick;
+    insertion.sort(arr1.begin(), arr1.end(),[](const int& a, const int& b){
+        return a>b;});
+    bubble.sort(arr2.begin(), arr2.end(), [](const int& a, const int& b){
         return a>b;
     });
-    // alg.sort(arr.begin(), arr.end());
-    for(auto i: arr){
-        std::cout<<i<<", ";
-    }
+    quick.sort(arr3.begin(), arr3.end(),[](const int& a, const int& b){
+        return a<b;});
+    printArr(arr1);
+    printArr(arr2);
+    printArr(arr3);
     return 0;
 }
